49b326004ce7ac04917ddb33fc6ff7d5f6376bff
Juan Young Huh <juan.huh@evry.com>
2020-03-15 15:22:33 +0100

    x

--- lib/components/main_app_bar.dart
+++ lib/components/main_app_bar.dart
@@ -19,25 +19,87 @@ class _MainAppBarState extends State<MainAppBar> {
           style: TextStyle(fontFamily: 'OpenSans', fontSize: 17.0, fontWeight: FontWeight.w600, letterSpacing: 1.5)),
       actions: <Widget>[
         if (widget.title == 'Feed')
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            showModalBottomSheet(context: context, builder: (context) {
-              return Container(
-                child: Text('hello'),
+          IconButton(
+            icon: Icon(Icons.tune),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return FilterModal();
+                },
               );
-            });
-          },
-        ),
+            },
+          ),
         Padding(
           padding: const EdgeInsets.only(right: 5.0),
           child: new IconButton(
             icon: Icon(Icons.exit_to_app),
-            onPressed: () async {
-            },
+            onPressed: () async {},
           ),
         )
       ],
+    );
+  }
+}
+
+class FilterChipEntry {
+  final String name;
+
+  const FilterChipEntry(this.name);
+}
+
+class FilterModal extends StatefulWidget {
+  @override
+  _FilterModalState createState() => _FilterModalState();
+}
+
+class _FilterModalState extends State<FilterModal> {
+  final List<FilterChipEntry> _cast = <FilterChipEntry>[
+    const FilterChipEntry('Grocery'),
+    const FilterChipEntry('Medicine'),
+    const FilterChipEntry('Others'),
+  ];
+
+  List<String> _filters = <String>[];
+
+  Iterable<Widget> get typeWidgets sync* {
+    for (FilterChipEntry entry in _cast) {
+      yield Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: FilterChip(
+          label: Text(entry.name),
+          selected: _filters.contains(entry.name),
+          onSelected: (bool value) {
+            setState(() {
+              if (value) {
+                _filters.add(entry.name);
+              } else {
+                _filters.removeWhere((String name) {
+                  return name == entry.name;
+                });
+              }
+            });
+          },
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Choose Type'),
+          Wrap(
+            children: typeWidgets.toList(),
+          )
+        ],
+      ),
     );
   }
 }
