5850494234bf9f5900a3f6909fa3ea2d71a28679
Juan Young Huh <juan.huh@evry.com>
2020-03-16 18:12:13 +0100

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,31 @@
 import 'package:divoc/screens/home_screen.dart';
+import 'package:divoc/screens/login_screen.dart';
+import 'package:divoc/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Divoc',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: 'OpenSans'
+    return MultiProvider(
+      providers: [
+        StreamProvider<FirebaseUser>.value(value: AuthService().user),
+      ],
+      child: MaterialApp(
+        title: 'Divoc',
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeScreen.id: (context) => HomeScreen(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: 'OpenSans',
+        ),
+        home: LoginScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
-
