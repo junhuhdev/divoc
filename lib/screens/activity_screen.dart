cf46e6674af66c4c44db5f28d86d90973948748f
Juan Young Huh <juan.huh@evry.com>
2020-03-17 23:04:33 +0100

    x

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -2,6 +2,7 @@ import 'package:divoc/common/loader.dart';
 import 'package:divoc/models/feed.dart';
 import 'package:divoc/services/feed_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -51,15 +52,45 @@ class ActivityCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Text(feed.name),
-              Text(feed.status),
-            ],
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(feed.name),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                if (feed.status == 'created') ...[
+                  Chip(
+                    backgroundColor: Colors.red,
+                    labelStyle: TextStyle(color: Colors.white),
+                    label: Text(feed.status),
+                  ),
+                ],
+                if (feed.status == 'pending') ...[
+                  Chip(
+                    backgroundColor: Colors.amber,
+                    labelStyle: TextStyle(color: Colors.white),
+                    label: Text(feed.status),
+                  ),
+                ],
+                Chip(
+                  labelPadding: EdgeInsets.symmetric(horizontal: 6.0),
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  label: Text(feed.category),
+                ),
+              ],
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward_ios),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
