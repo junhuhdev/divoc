64ad9500d09014dd741faf39335d3c3b30963810
Juan Young Huh <juan.huh@evry.com>
2020-03-18 23:46:06 +0100

    x

--- lib/screens/delivery_screen.dart
+++ lib/screens/delivery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:divoc/common/chips.dart';
+import 'package:divoc/common/list_tile.dart';
 import 'package:divoc/common/loader.dart';
 import 'package:divoc/models/feed.dart';
 import 'package:divoc/services/feed_service.dart';
@@ -63,58 +64,22 @@ class ActivityCard extends StatelessWidget {
     return InkWell(
       child: Card(
         elevation: 3.0,
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+        child: ListTile(
+          contentPadding: EdgeInsets.all(15.0),
+          subtitle: FeedListTileColumn(name: feed.name, created: feed.created, category: feed.category),
+          trailing: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.person, size: 18.0),
-                      SizedBox(width: 8.0),
-                      Text(feed.name),
-                    ],
-                  ),
-                  SizedBox(height: 5.0),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.place, size: 18.0),
-                      SizedBox(width: 8.0),
-                      Text("Kista, "),
-                      Text("Stockholm"),
-                    ],
-                  ),
-                  SizedBox(height: 5.0),
-                  Row(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today, size: 18.0),
-                      SizedBox(width: 8.0),
-                      Text(formatter.format(feed.created))
-                    ],
-                  ),
-                  SizedBox(height: 5.0),
-                  Row(
-                    children: <Widget>[Icon(Icons.category, size: 18.0), SizedBox(width: 8.0), Text(feed.category)],
-                  ),
-                ],
-              ),
-              Column(
-                children: <Widget>[
-                  if (feed.status == 'created') ...[
-                    FeedStatusChip(status: feed.status, backgroundColor: Colors.red),
-                  ],
-                  if (feed.status == 'pending') ...[
-                    FeedStatusChip(status: feed.status, backgroundColor: Colors.amber),
-                  ],
-                  if (feed.status == 'completed') ...[
-                    FeedStatusChip(status: feed.status, backgroundColor: Colors.green),
-                  ],
-                ],
-              ),
+              if (feed.status == 'created') ...[
+                FeedStatusChip(status: feed.status, backgroundColor: Colors.red),
+              ],
+              if (feed.status == 'pending') ...[
+                FeedStatusChip(status: feed.status, backgroundColor: Colors.amber),
+              ],
+              if (feed.status == 'completed') ...[
+                FeedStatusChip(status: feed.status, backgroundColor: Colors.green),
+              ],
             ],
           ),
         ),
