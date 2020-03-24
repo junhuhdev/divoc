1dfd42171091cec1922e789a2d6fa1e8d3f1c44c
Juan Young Huh <juan.huh@evry.com>
2020-03-24 16:01:19 +0100

    x

--- lib/screens/delivery_screen.dart
+++ lib/screens/delivery_screen.dart
@@ -1,9 +1,6 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:divoc/common/chips.dart';
-import 'package:divoc/common/constants.dart';
+import 'package:divoc/common/cards.dart';
 import 'package:divoc/common/form_container.dart';
 import 'package:divoc/common/form_field.dart';
-import 'package:divoc/common/list_tile.dart';
 import 'package:divoc/common/loader.dart';
 import 'package:divoc/components/feed/create_feed.dart';
 import 'package:divoc/components/maps/static_google_map.dart';
@@ -15,7 +12,6 @@ import 'package:divoc/services/db.dart';
 import 'package:divoc/services/feed_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class DeliveryScreen extends StatefulWidget {
@@ -47,8 +43,25 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
                 child: ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (context, index) {
-                    return DeliveryCard(
-                      feedRequest: snapshot.data[index],
+                    FeedRequest feedRequest = snapshot.data[index];
+                    return FeedListTileCard(
+                      image: feedRequest.image,
+                      name: feedRequest.name,
+                      status: feedRequest.status,
+                      city: feedRequest.city,
+                      state: feedRequest.state,
+                      created: feedRequest.created,
+                      onTap: () {
+                        if (feedRequest.status == 'requested') {}
+                        if (feedRequest.status == 'pending' || feedRequest.status == 'completed') {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DeliveryDetails(feedRequest: feedRequest),
+                            ),
+                          );
+                        }
+                      },
                     );
                   },
                 ),
@@ -60,77 +73,6 @@ class _DeliveryScreenState extends State<DeliveryScreen> {
     } else {
       return Container();
     }
-  }
-}
-
-class DeliveryCard extends StatelessWidget {
-  final FeedRequest feedRequest;
-
-  DeliveryCard({this.feedRequest});
-
-  final formatter = new DateFormat('EEE d MMM h:mm a');
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 8.0,
-      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
-      child: Container(
-        decoration: BoxDecoration(color: kCardColor),
-        child: ListTile(
-          contentPadding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
-          leading: Container(
-            padding: EdgeInsets.only(right: 12.0),
-            decoration: new BoxDecoration(border: new Border(right: new BorderSide(width: 1.0, color: Colors.white24))),
-            child: CircleAvatar(
-              backgroundImage: feedRequest.image == null ? Icon(Icons.person) : CachedNetworkImageProvider(feedRequest.image),
-              radius: 30.0,
-            ),
-          ),
-          title: Row(
-            children: <Widget>[
-              Expanded(
-                flex: 4,
-                child: Text(
-                  feedRequest.name,
-                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    // tag: 'hero',
-                    child: LinearProgressIndicator(
-                        backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
-                        value: getProgressStatus(feedRequest.status),
-                        valueColor: AlwaysStoppedAnimation(getColorStatus(feedRequest.status))),
-                  )),
-            ],
-          ),
-          isThreeLine: true,
-          subtitle: Wrap(
-            direction: Axis.vertical,
-            alignment: WrapAlignment.spaceBetween,
-            children: <Widget>[
-              Text("${feedRequest.state}, ${feedRequest.city}", style: TextStyle(color: Colors.white, fontSize: 12.0)),
-              SizedBox(height: 3),
-              Text(formatter.format(feedRequest.created), style: TextStyle(color: Colors.white70, fontSize: 12.0))
-            ],
-          ),
-          onTap: () {
-            if (feedRequest.status == 'requested') {}
-            if (feedRequest.status == 'pending' || feedRequest.status == 'completed') {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DeliveryDetails(feedRequest: feedRequest),
-                ),
-              );
-            }
-          },
-        ),
-      ),
-    );
   }
 }
 
