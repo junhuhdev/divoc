94408e9b632430a2368db948283ca052bdbaaa65
Jun Huh <45692017+junhuhdev@users.noreply.github.com>
2020-03-29 16:46:08 +0200

    x

--- lib/screens/user_profile_screen.dart
+++ lib/screens/user_profile_screen.dart
@@ -6,6 +6,7 @@ import 'package:divoc/common/loader.dart';
 import 'package:divoc/models/user.dart';
 import 'package:divoc/services/db.dart';
 import 'package:flutter/material.dart';
+import 'package:divoc/services/utils.dart';
 
 class UserProfileScreen extends StatefulWidget {
   final String userId;
@@ -28,36 +29,39 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<User>(
-        future: _user,
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            if (snapshot.hasError) {
-              print("Error: ${snapshot.error}");
-            }
-            return LoadingScreen();
+      future: _user,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          if (snapshot.hasError) {
+            print("Error: ${snapshot.error}");
           }
-          final User user = snapshot.data;
+          return LoadingScreen();
+        }
+        final User user = snapshot.data;
 
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).primaryColor,
-              title: Text(user.name),
-              centerTitle: true,
-            ),
-            body: FormContainer(
-              children: <Widget>[
-                UserProfileImage(image: user.photo),
-                GenericTextContainer(title: 'Namn', content: user.name, icon: Icons.person),
-                GenericTextContainer(title: 'Kön', content: user.gender, icon: CommunityMaterialIcons.gender_male_female),
-                GenericTextContainer(title: 'Ålder', content: user.getAge.toString(), icon: Icons.person),
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).primaryColor,
+            title: Text(user.name),
+            centerTitle: true,
+          ),
+          body: FormContainer(
+            children: <Widget>[
+              UserProfileImage(image: user.photo),
+              GenericTextContainer(title: 'Namn', content: user.name, icon: Icons.person),
+              GenericTextContainer(title: 'Ålder', content: user.getAge.toString(), icon: Icons.person),
+              GenericTextContainer(title: 'Kön', content: user.gender, icon: CommunityMaterialIcons.gender_male_female),
+              if (!user.state.isNullOrEmpty && !user.city.isNullOrEmpty) ...[
                 GenericTextContainer(
                   title: 'Plats',
                   content: '${user.state}, ${user.city}',
                   icon: Icons.place,
                 ),
               ],
-            ),
-          );
-        });
+            ],
+          ),
+        );
+      },
+    );
   }
 }
