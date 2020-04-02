3727bc08f1195af688f9afc9c8de83e326099333
Therese Abrahamsson <therese.abra@hotmail.com>
2020-04-02 18:33:08 +0200

    ios release

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,7 +11,24 @@ import GoogleMaps
   ) -> Bool {
     FirebaseApp.configure()
     GMSServices.provideAPIKey("AIzaSyDvqYtwG_DV0cV11j5R5pJmA4irT3OGAvc")
+    
+    if #available(iOS 10.0, *) {
+      // For iOS 10 display notification (sent via APNS)
+      UNUserNotificationCenter.current().delegate = self
 
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: {_, _ in })
+    } else {
+      let settings: UIUserNotificationSettings =
+      UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+      application.registerUserNotificationSettings(settings)
+    }
+
+    application.registerForRemoteNotifications()
+
+    
     GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
